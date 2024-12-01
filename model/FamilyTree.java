9be2a002644c161514fdf35fe9d937759cbad919
Kira <kirnan_t@outlok.com>
2024-12-01 16:38:42 +0700

    FT

---
+++ model/FamilyTree.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class FamilyTree implements Serializable, Iterable<Person> {
+    private static final long serialVersionUID = 1L;
+    private Map<String, Person> people;
+
+    public FamilyTree() {
+        this.people = new HashMap<>();
+    }
+
+    public void addPerson(Person person) {
+        this.people.put(person.getName(), person);
+    }
+
+    public Person findPerson(String name) {
+        return this.people.get(name);
+    }
+
+    public List<Person> getChildrenOf(String personName) {
+        Person person = findPerson(personName);
+        if (person != null) {
+            return person.getChildren();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Iterator<Person> iterator() {
+        return people.values().iterator();
+    }
+}
---
+++ model/Person.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Person implements Serializable, Comparable<Person> {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private Date birthDate; // Добавлено поле для даты рождения
+    private List<Person> children;
+
+    public Person(String name, Date birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.children = new ArrayList<>();
+    }
+
+    public void addChild(Person child) {
+        this.children.add(child);
+    }
+
+    public List<Person> getChildren() {
+        return this.children;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Date getBirthDate() {
+        return this.birthDate; // Геттер для даты рождения
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return this.name.compareTo(other.name); // Сравнение по имени
+    }
+}
